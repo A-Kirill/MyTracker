66df7bf521e0d5bcb1878ff123285a59addfd1fd
Kirill Anisimov <anisimov_k@icloud.com>
2020-09-03 21:32:47 +0300

    tracking of movement in the background mode + added routePath on map

--- MyTracker/App/Flows/MapViewController.swift
+++ MyTracker/App/Flows/MapViewController.swift
@@ -17,6 +17,9 @@ class MapViewController: UIViewController {
     private var marker: GMSMarker?
     private var locationManager: CLLocationManager?
     private var geocoder = CLGeocoder()
+    
+    var route: GMSPolyline?
+    var routePath: GMSMutablePath?
 
     @IBOutlet weak var mapView: GMSMapView!
     
@@ -39,8 +42,12 @@ class MapViewController: UIViewController {
     
     func configureLocationManager() {
         locationManager = CLLocationManager()
-        locationManager?.requestWhenInUseAuthorization()
         locationManager?.delegate = self
+        locationManager?.allowsBackgroundLocationUpdates = true
+        locationManager?.pausesLocationUpdatesAutomatically = false
+        locationManager?.startMonitoringSignificantLocationChanges()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.requestWhenInUseAuthorization()
     }
     
     
@@ -59,6 +66,17 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func goTo(_ sender: UIBarButtonItem) {
+        // Unlinking the old line from the map
+        route?.map = nil
+        // Replace the old line with a new one
+        route = GMSPolyline()
+        // Replace the old path with a new one, while empty (no points)
+        routePath = GMSMutablePath()
+        route?.strokeColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        route?.strokeWidth = 8
+        // Add a new line to the map
+        route?.map = mapView
+        // Start tracking or continue if it is already running
         locationManager?.startUpdatingLocation()
     }
     
@@ -84,19 +102,16 @@ extension MapViewController: GMSMapViewDelegate {
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        guard let myLocation = locations.first else { return }
-        let updateCoordinate = myLocation.coordinate
-        let camera = GMSCameraPosition(target: updateCoordinate, zoom: defaultZoom)
+        // Take the last point from the resulting set
+        guard let location = locations.last else { return }
+        // Add it to the route path
+        routePath?.add(location.coordinate)
+        // Updating the path along the route line by reassigning
+        route?.path = routePath
         
-        mapView.animate(to: camera)
-        
-        let marker = GMSMarker(position: updateCoordinate)
-        marker.icon = GMSMarker.markerImage(with: .blue)
-        marker.map = mapView
-        
-        geocoder.reverseGeocodeLocation(myLocation) { place, error in
-            print(place?.first as Any)
-        }
+        // To see the movement, set the camera to the point we just added
+        let position = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: defaultZoom)
+        mapView.animate(to: position)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
--- Podfile
+++ Podfile
@@ -4,7 +4,10 @@
 target 'MyTracker' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
+
 pod 'GoogleMaps'
+pod 'RealmSwift'
+
   # Pods for MyTracker
 
 end
